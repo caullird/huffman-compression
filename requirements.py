da0614f3a70bf660769b8eee6d46b33f7d278a7c
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-03-03 19:24:36 +0100

    update requirements

--- requirements.py
+++ requirements.py
@@ -2,6 +2,6 @@
 
 #Link to files 
 
-data_need_to_be_convert = "data/initial_data/"
-data_converted = "data/analyzed_data/"
+initial_data = "data/initial_data/"
+result_data = "data/result_data/"
 data_lexique_freq = "data/lexique_data/"

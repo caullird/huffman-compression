07f7378d5e7d59e9bd09a2db90959d4284209f9e
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-03-03 17:25:30 +0100

    Update tree management

--- sample/tree_management.py
+++ sample/tree_management.py
@@ -1,3 +1,90 @@
+import sample.node_management as node_management
+from operator import attrgetter
+
+
+class HuffmanTree:
+
+	def create_node(self, data,left_child = None, right_child = None):
+		if(right_child is None):
+
+			if(left_child is None):
+				#None sans aucun enfant
+				return node_management.Node(data[0],data[1])
+
+			#Node avec uniquement la node de gauche 
+			return node_management.Node(data[0],data[1],left_child)
+
+		#Node avec gauche et droite
+		return node_management.Node(data[0],data[1],left_child,right_child)
+
+	def create_huffman_tree(self, list_tupple):
+		list_node = []
+
+		#Ajoute de tout les tupples en tant que noeud 
+		for tupple in list_tupple:
+			list_node.append(self.create_node(tupple))
+
+		#On modifie l'ensemble des noeuds 
+		while(len(list_node) > 1):
+			list_node.sort(key=attrgetter('frequence'))
+
+			#On prend les deux plus petits et on les supprimes
+			n1 = list_node.pop(0)
+			n2 = list_node.pop(0)
+
+			#On crée le nouveau noeud 
+			new_node = self.create_node(("NodeKey",n1.get_frequence() + n2.get_frequence()),n2,n1)
+			list_node.append(new_node)	
+
+			#On ajoute les correspondances 1/0 pour le futur traitement binaire 
+			self.add_node_value(list_node[0])
+
+		return list_node[0]
+
+
+	def add_node_value(self, node):
+		left_child,right_child = node.get_left_child(),node.get_right_child()
+
+		#Si c'est à gauche, on met 1
+		if(left_child != None):
+			left_child.set_value("1")
+			self.add_node_value(left_child)
+
+		#Si c'est à droite, on met 0
+		if(right_child != None):
+			right_child.set_value("0")
+			self.add_node_value(right_child)
+
+
+
+	#Fonction pour compression en différents formats, binaire....
+	def compress_text(self,node,text):
+		alphabet_compress = self.get_alphabet_compress(node)
+
+		result_bin = ""
+
+		for letter in text:
+			result_bin += alphabet_compress[letter]
+		
+		print(result_bin)
+
+
+	#Correspondance alphabet to binaire
+	def get_alphabet_compress(self,node):
+		list_chemin = {}
+
+		for leaf in node.get_leaf_list():
+			string = ""
+
+			for chemin in node.get_chemin_vers_node(leaf):
+				if(chemin.get_value() != None):
+					string += chemin.get_value()
+
+			list_chemin[chemin.get_label()] = string
+
+
+		return list_chemin
+		
 
 
 

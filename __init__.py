1309336a7fba8290c068caab553fa6a9cc747912
CAULLIREAU Dorian <caullireau.dorian@gmail.com>
2021-03-03 17:25:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-import sample.lex_management as lex_management
+import sample.freq_management as freq_management
 import sample.file_management as file_management
 import sample.tree_management as tree_management
 import sample.node_management as node_management
@@ -14,15 +14,16 @@ if __name__ == "__main__":
 	text = file_management.File(require.data_need_to_be_convert + config.FILE_NAME).open_file().read()
 
 
-	text_frequences = lex_management.Frequence(text).calcul_frequence()
+	text_frequences = freq_management.Frequence(text).get_frequence()
 
+	print(text_frequences)
 
 	file_management.File(require.data_lexique_freq + "lex_" + config.FILE_NAME).write_file(text_frequences)
 
 
+	huffman_tree = tree_management.HuffmanTree().create_huffman_tree(text_frequences)
 
-	print(text_frequences)
-
+	bin_textsimple = tree_management.HuffmanTree().compress_text(huffman_tree,text)
 
 
 

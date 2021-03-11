d3bcba9a5a302aa2980090384e04660623f326bc
caullird <caullireau.dorian@gmail.com>
2021-03-11 20:16:12 +0100

    Update HuffmanCompress

--- main_program/HuffmanCompress.py
+++ main_program/HuffmanCompress.py
@@ -10,13 +10,25 @@ import requirements as require
 class HuffmanCompress:
 
 	def __init__(self,data):
-		self.data = self.initilize_data(data)
+		self.paramater = data
+		self.text = self.initilize_data(data)
 		self.is_file = False
 		self.compress = self.compress()
 
-	def sef_is_file(self,data):
+	def set_is_file(self,data):
 		self.is_file = data
 
+	def set_data_name(self,data):
+		self.data_name = data
+
+	def set_paramater(self,data):
+		self.paramater = data
+
+	def getDataName(self):
+		if(self.is_file):
+			return self.paramater
+		else:
+			return self.paramater[0:10]
 
 	def initilize_data(self,data):
 
@@ -25,7 +37,6 @@ class HuffmanCompress:
 		if(file.file_exist()):
 			self.set_is_file(True)
 			return file.open_file()
-		
 		return data
 
 
@@ -33,11 +44,12 @@ class HuffmanCompress:
 
 		# Gestion de la fréquence & écriture dans un fichier lex_ : 
 
-		freq = Frequence(self.data)
+		freq = Frequence(self.text)
 
 		text_frequences = freq.get_frequence()
 
-		file_freq = File(require.data_lexique_freq + "lex_" + self.data)
+
+		file_freq = File(require.data_lexique_freq + "lex_" + self.text)
 
 		file_freq.write_file_freq(text_frequences,self.is_file)
 
@@ -51,14 +63,40 @@ class HuffmanCompress:
 
 		compress = Compress()
 
-		bin_textsimple,alphabet_binaire = compress.compress_text(huffman_tree,self.data)
+		bin_textsimple,alphabet_binaire = compress.compress_text(huffman_tree,self.text)
 
-		#TODO ///////////////////////////// fichier_compress = file_management.File(require.result_data + "result_" + config.FILE_NAME).write_file(bin_textsimple)
+		fichier_compress = File(require.result_data + "result_" + self.text).write_file(bin_textsimple)
 
 		#Gestion du ratio de compression
 
-		ratio = Ratio(alphabet_binaire,text_frequences,self.data)
+		ratio = Ratio(alphabet_binaire,text_frequences,self.text)
 
 		ratio_percent = ratio.get_precent_save()
 
+		nb_moyen = ratio.nombre_moyen()
+
+		self.write_result(text_frequences,bin_textsimple,alphabet_binaire,ratio_percent,nb_moyen)
+
+	def write_result(self,text_frequences,bin_textsimple,alphabet_binaire,ratio_percent,nb_moyen):
+		print("\n")
+		print("##############################################")
+		print("Resultat pour le texte suivant : ")
+		print(self.text)
+		print("##############################################")
+		print("Alphabet de fréquence :")
+		print(text_frequences)
+		print("##############################################")
+		print("Alphabet binaire :")
+		print(alphabet_binaire)
+		print("##############################################")
+		print("Conversion du texte en binaire :")
+		print(bin_textsimple)
+		print("##############################################")
+		print("Ratio de compression :")
 		print(ratio_percent)
+		print("##############################################")
+		print("Nombre moyen de bits de stockage :")
+		print(nb_moyen)
+		print("##############################################")
+
+

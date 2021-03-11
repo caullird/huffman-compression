61e6d3624fe6461247d18b10c47c5365ddba6b63
caullird <caullireau.dorian@gmail.com>
2021-03-11 20:23:50 +0100

    Create task7_unit_test branch

---
+++ tests/HuffmanTest.py
@@ -0,0 +1,19 @@
+import unittest
+
+from config.File import File
+
+from main_program.Frequence import Frequence
+from main_program.HuffmanTree import HuffmanTree
+from main_program.Compress import Compress
+from main_program.Ratio import Ratio
+
+import requirements as require
+
+
+
+class HuffmanTest(unittest.TestCase):
+	def test_frequence(self):
+		self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()

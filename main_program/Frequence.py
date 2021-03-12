5ad01749877c7f46c3a070dd6e3a688675fb13ed
caullird <caullireau.dorian@gmail.com>
2021-03-12 20:07:17 +0100

    Add comments in Frequence

--- main_program/Frequence.py
+++ main_program/Frequence.py
@@ -1,8 +1,50 @@
 
-class Frequence :
-	def __init__(self,text,frequence = 0):
+# Import FrequenceInterface related to the current class
+from main_program.Interface.FrequenceInterface import FrequenceInterface
+
+class Frequence(FrequenceInterface) :
+
+	""" Class Frequence 
+
+	@override : FrequenceInterface
+
+	Used to initialise a frequency object to determine a frequency table
+
+    """
+
+
+	def __init__(self, text:str, frequence:int = 0):
+
+		""" Constructor of the Frequence class
+
+	    Parameters
+	    ----------
+	    text : str
+	    	Text in string form given by the user
+	    frequence : int
+	        variable that contains the frequency related to the parameter given by the user
+
+	    """
+
 		self.text = text 
-		self.frequence = self.calcul_frequence()
+		self.frequence = self.calcul_frequence(text)
+
+
+	""" Getters & Setters
+
+    Function allowing the modification and recovery of the elements of the object
+
+    Getters
+    ----------
+    	- get_text(self) : is used to retrieve the "text" element
+    	- get_frequence(self) : is used to retrieve the "frequence" element 
+
+    Setters 
+    ----------
+    	- set_frequence(self,value) : is used to modify the value of the "frequence" element
+    	- set_text(self,value) : is used to modify the value of the "text" element
+        
+    """
 
 	def get_text(self):
 		return self.text
@@ -10,22 +52,38 @@ class Frequence :
 	def get_frequence(self):
 		return self.frequence
 
-	def set_text(self, value):
+	def set_text(self, value:str):
 		self.text = value
 
-	def set_frequence(self, value):
+	def set_frequence(self, value:dict):
 		self.frequence = value
 
-	def calcul_frequence(self):
+	def calcul_frequence(self, text:str):
+
+		"""Used to determine the frequency from a given string of characters
+
+		@override : calcul_frequence(self,text:str) in FrequenceInterface
+
+	    Parameters
+	    ----------
+	    text : string
+	        Character string given by the user
+
+	    Returns
+	    -------
+	    Dictionary
+	        Dictionary with the corresponding letter frequency 
+	    """
+
 		freq = {}
 
-		for c in self.text:
+		for c in text:
 		    if c in freq:
 		        freq[c] += 1
 		    else:
 		        freq[c] = 1
 
-		freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+		freq = sorted(freq.items(), key = lambda x: x[1], reverse = True)
 
 		self.set_frequence(freq)
 

923a9d9bf72d0e14601d161838bf2d74c422c88a
caullird <caullireau.dorian@gmail.com>
2021-03-12 22:46:47 +0100

    Add comment in File Class

--- config/File.py
+++ config/File.py
@@ -1,28 +1,92 @@
-import os.path
+# Import of external libraries
 from os import path
 
-class File:
+# Importation of interfaces
+from config.Interface.FileInterface import FileInterface
 
-	def __init__(self,file):
+class File(FileInterface):
+
+	""" Class File
+
+	@override : FileInterface
+
+	Global file management
+
+    """
+
+	def __init__(self, file):
+
+		""" Constructor of the File class
+
+	    Parameters
+	    ----------
+	    file : global file to be processed
+
+	    """
 		self.file = file 
+
+
+	""" Getters & Setters
+
+    Function allowing the modification and recovery of the elements of the object
+
+    Getters 
+    ----------
+    	- get_file(self) : is used to retrieve the "file" element 	
+
+    """
 
 	def get_file(self):
 		return self.file
 
-	def get_file(self, value):
-		self.file = value
 
 	def open_file(self):
+
+		"""Allows you to open a file
+
+		@override : open_file(self) in FileInterface
+
+	    Returns
+	    -------
+	    file 
+	        Returns the content of the file
+	    """
+
 		return open(self.file, "r").read()
 
 
 	def file_exist(self):
+
+		"""Allows to check if the file exists or not
+
+		@override : file_exist(self) in FileInterface
+
+	    Returns
+	    -------
+	    boolean
+	        True if it is a file, False if it is not a file
+	    """
+
 		if(path.exists(self.file)):
 			return True
 		
 		return False 
 
-	def write_file_freq(self,data,is_file = False):
+	def write_file_freq(self,data: dict ,is_file: bool = False):
+
+
+		"""Allows the writing of frequencies in a file
+
+		@override : write_file_freq(self,data: dict ,is_file: bool) in FileInterface
+
+		Parameters
+	    ----------
+	    data : dict
+	        Dictionary of frequencies according to the characters
+	    is_file : bool 
+	    	Allows to know if it is a file or not
+
+	    """
 
 		if(is_file):
 			f = open(self.file,"w")
@@ -35,7 +99,21 @@ class File:
 			f.write(str(value[0]) + " " + str(value[1]) + "\n")
 		f.close()
 
-	def write_file(self,data):
+
+	def write_file(self,data: str):
+
+
+		"""Allows to write to the file in other cases (other than frequency)
+
+		@override : write_file(self,data: str) in FileInterface
+
+		Parameters
+	    ----------
+	    data : string
+	        Character string given by the user
+
+	    """
+
 		f = open(self.file + ".txt","w+")
 
 		f.write(data)

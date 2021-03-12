8b01c361995f561f10aa170669f7806a550f4063
caullird <caullireau.dorian@gmail.com>
2021-03-12 23:02:46 +0100

    Create RatioInterface.py

---
+++ main_program/Interface/RatioInterface.py
@@ -0,0 +1,33 @@
+
+class RatioInterface:
+
+	def calc_ratio(self):
+
+		"""Allows you to determine the compression ratio
+
+		Use in Ratio class
+
+	    Returns
+	    -------
+	    ratio : float
+	    	- Ratio corresponding to the compression rate on the initial rate
+
+	    """
+
+		pass
+
+	
+	def character_avg(self):
+
+		"""Allows to calculate the average weight of the characters
+
+		Use in Ratio class
+
+	    Returns
+	    -------
+	    avg : float
+	    	average bits per character
+	        
+	    """
+
+		pass

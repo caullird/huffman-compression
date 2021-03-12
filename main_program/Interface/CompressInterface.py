af8a210e9931933b8fa306517a69887cb042bfdf
caullird <caullireau.dorian@gmail.com>
2021-03-12 23:01:52 +0100

    Create CompressInterface.py

---
+++ main_program/Interface/CompressInterface.py
@@ -0,0 +1,53 @@
+
+class CompressInterface:
+
+	""" Interface CompressInterface 
+
+	Use Compress Class
+
+	Used to compress a string given by the user
+
+    """
+
+	def compress_text(self,node:object,text:str):
+
+		"""Used to compress a text in binary format
+
+		Use in Compress
+		
+	    Parameters
+	    ----------
+	    node : object
+	    text : string 
+	    	Text that needs to be analyzed
+
+	    Returns
+	    -------
+	    result_bin : string
+	        translation into binary
+	    alphabet_compress : Dictionary 
+			Dictionary of correspondence of each character
+
+	    """
+
+		pass
+
+
+	def get_alphabet_compress(self,node:object):
+
+		"""Allows you to determine the matching dictionary 
+
+		Use in Compress
+		
+	    Parameters
+	    ----------
+	    node : object
+
+	    Returns
+	    -------
+	    dictionnaire_binary : dictionary
+	        Dictionary of correspondence of each character
+
+	    """
+
+		pass

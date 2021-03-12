4f2cf5f28b7524f2f968e22cdd42096b4ae9912a
caullird <caullireau.dorian@gmail.com>
2021-03-12 23:02:40 +0100

    Create NodeInterface.py

---
+++ main_program/Interface/NodeInterface.py
@@ -0,0 +1,66 @@
+
+class NodeInterface:
+
+
+	def is_leaf(self):
+
+		"""Allows to know if it is a leaf or not
+
+		Use Node Class
+
+	    Returns
+	    -------
+	    boolean
+	    	Returns true if it is a leaf, or false otherwise
+	    """
+
+		pass
+
+	def get_node_list(self):
+
+		"""Allows you to determine the list of nodes
+
+		Use Node Class
+
+	    Returns
+	    -------
+	    liste_node
+	    	- allows you to retrieve the list of nodes
+	        
+	    """
+
+		pass
+
+	def get_leaf_list(self):
+
+		"""Allows you to determine the list of leaf
+
+		Use Node Class
+
+	    Returns
+	    -------
+	    list_leaf
+	    	- allows you to retrieve the list of leaf
+	        
+	    """
+
+		pass
+
+	def get_chemin_vers_node(self,default_node:object):
+
+		"""Allows to determine the path to the node
+
+		Use Node Class
+
+		Parameters
+	    ----------
+	    default_node : object
+
+	    Returns
+	    -------
+	    list_leaf
+	    	- allows you to retrieve the list of leaf
+	        
+	    """
+
+		pass
